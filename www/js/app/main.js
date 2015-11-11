6d27d1cb28896813ccf42a66e5821d1427eb512f
Rodrigo <rodrigo.garcia@engrande.com>
2015-11-11 11:57:52 +0100

    broken example

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,6 +1,9 @@
-define(["jquery", "jquery.alpha", "jquery.beta"], function($) {
-    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
-    $(function() {
-        $('body').alpha().beta();
-    });
+define([
+	"jquery", "alert", "jquery.alpha", "jquery.beta"
+], function ($, Alert) {
+	//the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+	$(function () {
+		$('body').alpha().beta();
+		Alert('hola');
+	});
 });
---
+++ www/js/lib/alert.js
@@ -0,0 +1,8 @@
+/**
+ * This should be an AMD compatible Alert class to trigger alerts
+ * @param {type} text
+ * @returns {undefined}
+ */
+var Alert = function (text) {
+	alert(text);
+}
